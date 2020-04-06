72ee5339847107b17f29c740069c736ed9bd502a
janbae <46355185+janbae@users.noreply.github.com>
2020-04-06 10:51:59 -0400

    commit to master
    just added spring starter project for category

---
+++ JanelleB_SadiaR_Comp303_Assgn4_Category/src/main/java/com/spring/microservice/category/JanelleBSadiaRComp303Assgn4CategoryApplication.java
@@ -0,0 +1,13 @@
+package com.spring.microservice.category;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JanelleBSadiaRComp303Assgn4CategoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JanelleBSadiaRComp303Assgn4CategoryApplication.class, args);
+	}
+
+}
