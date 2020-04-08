917629d520262997062b9c8351e54fc61e2a7762
janbae <46355185+janbae@users.noreply.github.com>
2020-04-08 03:36:06 -0400

    commit to master

--- JanelleB_SadiaR_Comp303_Assgn4_Category/src/main/java/com/spring/microservice/category/CategoryRepository.java
+++ JanelleB_SadiaR_Comp303_Assgn4_Category/src/main/java/com/spring/microservice/category/CategoryRepository.java
@@ -1,7 +1,10 @@
 package com.spring.microservice.category;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.data.jpa.repository.*;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
 
+	@Query("SELECT c FROM Category c WHERE c.jobCatId = ?1")
+	Category findCategoryById(int jobcatid);
 }
