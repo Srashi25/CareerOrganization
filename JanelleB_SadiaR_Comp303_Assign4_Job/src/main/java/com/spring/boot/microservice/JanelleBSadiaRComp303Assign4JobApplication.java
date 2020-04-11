774a36f663ac775867793e8bdd79bd8d33067038
Sadia Rashid <sadia.rashid1801@gmail.com>
2020-04-11 16:22:06 -0400

    commit

---
+++ JanelleB_SadiaR_Comp303_Assign4_Job/src/main/java/com/spring/boot/microservice/JanelleBSadiaRComp303Assign4JobApplication.java
@@ -0,0 +1,14 @@
+package com.spring.boot.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JanelleBSadiaRComp303Assign4JobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JanelleBSadiaRComp303Assign4JobApplication.class, args);
+		 System.out.println("Spring Boot JPA started-Job");
+	}
+
+}
---
+++ JanelleB_SadiaR_Comp303_Assign4_Job/src/main/java/com/spring/boot/microservice/JobService.java
@@ -0,0 +1,39 @@
+package com.spring.boot.microservice;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JobService {
+	
+	@Autowired
+	private JobRepository jobRepository;
+	
+	public List<Job> getAll() {
+		return jobRepository.findAll();
+	}
+	
+	public Optional<Job> getJobById(final int id) {
+		return jobRepository.findById(id);
+	}
+	
+	public Job updateJob(final int id, final Job job) {
+		return jobRepository.save(job);
+	}
+	
+	public Job saveJob(final Job job) {
+		return jobRepository.save(job);
+	}
+	
+	public void deleteJob(final int id) {
+		jobRepository.deleteById(id);
+	}
+	public Job findOneById(final int id) {
+		return jobRepository.findOneById(id);
+	}
+	
+
+}
